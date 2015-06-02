c8c37eacc6593b4d71bd3e788d58a20d383af4db
Tim Riffe <tim.riffe@gmail.com>
2015-06-01 18:04:53 -0700

    update as Dima suggested

--- TR1/R/Installation.R
+++ TR1/R/Installation.R
@@ -4,7 +4,7 @@ library(devtools)
 install_github("timriffe/TR1/TR1/HMDHFDplus")
 library(HMDHFDplus)
 USA <- readHMDweb(CNTRY = "USA",item = "mltper_1x1", username = us, password = pw)
-
+head(USA)
 
 
 
